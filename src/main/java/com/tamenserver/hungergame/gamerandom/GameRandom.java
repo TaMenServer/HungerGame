17bcf1263b6e16875e2059f3539d0688ba5a8bb0
TaMenServer <TaMenServer@gmail.com>
2017-01-26 18:17:14 +0800

    Update

--- src/main/java/com/tamenserver/hungergame/gamerandom/GameRandom.java
+++ src/main/java/com/tamenserver/hungergame/gamerandom/GameRandom.java
@@ -8,14 +8,15 @@ import org.bukkit.inventory.ItemStack;
 
 
 public class GameRandom {
-    private static Material[] mlist=new Material[]{Material.APPLE,Material.MUSHROOM_SOUP,Material.BREAD,Material.PORK,Material.RAW_FISH,Material.COOKED_FISH,Material.CAKE,Material.COOKIE,Material.MELON,Material.RAW_BEEF,Material.COOKED_BEEF,Material.RAW_CHICKEN,Material.COOKED_CHICKEN,Material.RABBIT,Material.COOKED_RABBIT};
+    private static Material[] mlist=new Material[]{Material.LOG,Material.BREAD,Material.ROTTEN_FLESH,Material.WOOD_AXE,Material.WOOD_SWORD,Material.LEATHER_HELMET,Material.LEATHER_CHESTPLATE,Material.LEATHER_BOOTS,Material.LEATHER_LEGGINGS};
     private Inventory tmpIn;
     public GameRandom(Inventory i){
         this.tmpIn=i;
         int nums=(int) (new Random().nextInt(20));
         for(int thisnum=1;thisnum<=nums;thisnum++){
-            int num=(int) (new Random().nextInt(15));
-            i.setItem(thisnum, new ItemStack(mlist[num],1));
+            int num=(int) (new Random().nextInt(this.mlist.length));
+            if(num==1) i.setItem(thisnum, new ItemStack(mlist[num],1));
+            else    i.setItem(thisnum, new ItemStack(mlist[num],16));
         }
         
     }
