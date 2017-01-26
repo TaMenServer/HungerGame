4521eda3981fd5025286f7f695f558ef291fc910
TaMenServer <TaMenServer@gmail.com>
2017-01-26 20:46:13 +0800

    update

--- src/main/java/com/tamenserver/hungergame/gamerandom/GameRandom.java
+++ src/main/java/com/tamenserver/hungergame/gamerandom/GameRandom.java
@@ -10,10 +10,10 @@ import org.bukkit.inventory.ItemStack;
 public class GameRandom {
     private static Material[] mlist=new Material[]{Material.LOG,Material.BREAD,Material.ROTTEN_FLESH,Material.WOOD_AXE,Material.WOOD_SWORD,Material.LEATHER_HELMET,Material.LEATHER_CHESTPLATE,Material.LEATHER_BOOTS,Material.LEATHER_LEGGINGS};
     public GameRandom(Inventory i){
-        int nums=(int) (new Random().nextInt(20));
+        int nums=(int) (new Random().nextInt(10));
         for(int thisnum=1;thisnum<=nums;thisnum++){
             int num=(int) (new Random().nextInt(GameRandom.mlist.length));
-            if(num==1) i.setItem(thisnum, new ItemStack(mlist[num],1));
+            if(num!=0) i.setItem(thisnum, new ItemStack(mlist[num],1));
             else    i.setItem(thisnum, new ItemStack(mlist[num],16));
         }
         
