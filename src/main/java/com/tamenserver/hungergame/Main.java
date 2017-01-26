217e10aea29227466bbc2ea3a9299205c489829f
TaMenServer <TaMenServer@gmail.com>
2017-01-26 19:26:31 +0800

    Update

--- src/main/java/com/tamenserver/hungergame/Main.java
+++ src/main/java/com/tamenserver/hungergame/Main.java
@@ -2,6 +2,18 @@ package com.tamenserver.hungergame;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.tamenserver.hungergame.command.CommandHungerGame;
+
 public class Main extends JavaPlugin {
+    @Override
+    public void onEnable(){
+        getCommand("hungergame").setExecutor(new CommandHungerGame(this));
+        this.getLogger().info("HungerGame加载完成！");
+    }
     
+    @Override
+    public void onDisable(){
+        this.getServer().getScheduler().cancelTasks(this);
+        this.getLogger().info("HungerGame卸载完成！");
+    }
 }
